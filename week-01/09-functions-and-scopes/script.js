745e95a335cc0dc00910d9f206352fd5895f5922
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-01 14:42:31 +0100

    update: 09

--- week-01/09-functions-and-scopes/script.js
+++ week-01/09-functions-and-scopes/script.js
@@ -1,5 +1,20 @@
 console.log("hi");
 
+function cal(val1, val2, op) {
+  switch (op) {
+    case "+":
+      return val1 + val2;
+    case "-":
+      return val1 - val2;
+    case "*":
+      return val1 * val2;
+    case "/":
+      return val1 / val2;
+  }
+}
+
+console.log(cal("2", "2", "*"));
+
 function doThis(text) {
   let addText = "!";
   let temp = `${text}${addText}`;
