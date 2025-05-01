7d12a9de873ba4916bfe78927976e0ac4ff5216d
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-01 09:57:27 +0100

    update: updated 07 js file

--- week-01/07-intro-to-javascript/script.js
+++ week-01/07-intro-to-javascript/script.js
@@ -1,5 +1,63 @@
 console.log("testing");
 
 let myString = "test";
+console.log(myString);
+
 let myNumber = 4;
+console.log(myNumber);
+
 let myBoolean = true;
+console.log(myBoolean);
+
+let a = 4;
+let b = 6;
+let c = a + b;
+console.log(c);
+
+console.log(a + " " + b);
+c = b;
+b = a;
+a = c;
+console.log(a + " " + b);
+
+let text1 = "hello ";
+let text2 = "world!";
+console.log(text1 + text2);
+let textGrope = `${text1} ${text2}`;
+console.log(textGrope);
+console.log(`${text1} ${text2}`);
+
+let coolText = "I am cool!";
+console.log(`${coolText} is ${coolText.length} long.`);
+
+let isTrue = true;
+console.log(isTrue);
+isTrue = !isTrue;
+console.log(isTrue);
+
+let thisString = "text";
+console.log(typeof thisString);
+let thisNumber = 8;
+console.log(typeof thisNumber);
+let thisBoolean = true;
+console.log(typeof thisBoolean);
+
+let noData;
+console.log(noData);
+let isNull = null;
+console.log(isNull);
+
+let thisNumber1 = 2;
+let thisNumber2 = 7;
+let biger = thisNumber1 > thisNumber2;
+console.log(biger);
+let tiney = thisNumber1 < thisNumber2;
+console.log(tiney);
+let equ1 = thisNumber1 == thisNumber2;
+console.log(equ1);
+let equ2 = thisNumber1 === thisNumber2;
+console.log(equ2);
+
+let proG = "javaScript";
+let day = 4;
+console.log(`I love programing in ${proG} and we are on day ${day}.`);
