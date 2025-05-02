be4ac650cb2f1bf79d8d50ab6a387b74a4fb09ce
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-02 16:13:55 +0100

    update: updated 10 and 06

--- week-01/10-the-DOM/script.js
+++ week-01/10-the-DOM/script.js
@@ -1,1 +1,16 @@
 console.log("hi");
+
+const thisText = document.getElementById("this-text");
+thisText.textContent = "it works";
+
+const pText = document.createElement("p");
+pText.textContent = "this is new";
+document.body.appendChild(pText);
+
+thisText.remove();
+
+const myButton = document.getElementById("my-button");
+
+myButton.addEventListener("click", () => {
+  console.log("it works!");
+});
