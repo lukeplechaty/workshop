17f20ba60240a93ddc0514717bc29d9bb4380342
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-20 10:48:28 +0100

    add: added supabase

---
+++ week-04/03-supabase/server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.listen(8080, () => {
+  console.log("server running on 8080");
+});
+
+const db = new pg.Pool({ connectionString: ProcessingInstruction.env.DB_URL });
+
+// app.get("/", (request, response) => {});
+// app.post("/", (request, response) => {});
